c12790956c1ee3f84a90a14dc0f53854a0ce49f2
luca.Angrisani <gab.angrisani@gmail.com>
2022-11-06 19:23:49 +0100

    interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { NeodeModule } from 'neode-nestjs';
+import { TransformInterceptor } from './app.interceptor';
 import { CalculateService } from './generator/calculate.service';
 import { GeneratorModule } from './generator/generator.module';
 import { UserModule } from './user/user.module';
@@ -13,6 +14,7 @@ import { UserModule } from './user/user.module';
   controllers: [],
   providers: [
     CalculateService,
+    TransformInterceptor
   ],
 })
 export class AppModule { }
